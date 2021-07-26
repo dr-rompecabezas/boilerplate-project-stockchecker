577d88be4fa050c5758c19333ffbae94ef1176b8
Felipe Villegas <felavid@gmail.com>
2021-07-26 12:55:56 -0400

    Added default likes value to schema.

--- models/stock.model.js
+++ models/stock.model.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const StockSchema = new mongoose.Schema({
     stock: { type: String, required: true, unique: true },
     ip: [{ type: String }],
-    likes: { type: Number }
+    likes: { type: Number, default: 0 }
 })
 
 // Export Model Constructor
